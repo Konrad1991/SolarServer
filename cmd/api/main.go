b99acc7573a95aee671518457683f5e18e77481b
Konrad1991 <konrad_kraemer@yahoo.de>
2024-07-31 16:57:26 +0200

    todo plan

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,29 +1,31 @@
 package main
 
 import (
-	"SolarServer/internal/server"
 	"SolarServer/internal/scandir"
+	"SolarServer/internal/server"
 	"fmt"
-  "strings"
-  "os"
+	"os"
+	"strings"
 )
 
 func main() {
-  head := &scandir.Tree{
-    Name:  "/home/konrad/Documents/ast2ast",
-    Files: nil,
-    Nexts:  nil,
-    Previous: nil,
-  }
-  scandir.Scan(head, "", head.Name)
-   scandir.Print("", head)
+	head := &scandir.Tree{
+		Name:     "/home/konrad/Documents/ast2ast",
+		Files:    nil,
+		Nexts:    nil,
+		Previous: nil,
+	}
+	scandir.Scan(head, "", head.Name)
+	scandir.Print("", head)
 
-  var sb strings.Builder
-  res := scandir.TreeToJson("", head, &sb)
-  fmt.Println(res)
-  d1 := []byte(res)
-  os.WriteFile("/home/konrad/Documents/testFile.json", d1, 0644)
-  return;
+	var sb strings.Builder
+	res := scandir.TreeToJson("", head, &sb)
+	fmt.Println(res)
+	d1 := []byte(res)
+	os.WriteFile("/home/konrad/Documents/testFile.json",
+		d1,
+		0644)
+	return
 	server := server.NewServer()
 
 	err := server.ListenAndServe()
